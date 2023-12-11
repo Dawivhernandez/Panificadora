31a3a80845d5d5849091bcf94c246bd0b6ce1a45
Dawivhernandez <93896309+Dawivhernandez@users.noreply.github.com>
2023-12-11 00:54:55 -0600

    Add files via upload

--- keys.js
+++ keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'panificadora'
+        host: 'ls-16dd6cf1efca1bc7d72146b9838031493d267624.ck40t0rbbtq8.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'dbmasteruser',
+        password: '0tpFPt60!CpheGSjR[b~2QNo6Tj)5])r',
+        database: 'Panificadora'
     }
 };
